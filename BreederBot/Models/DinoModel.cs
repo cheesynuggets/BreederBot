fe6172b9c2cf6f7dcd3239b5bdd964efdeea41e0
peeter banaan <rivottt@gmail.com>
2019-08-16 19:58:43 +0300

    Added data models for Dinos

---
+++ BreederBot/Models/DinoModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreederBot.Models
+{
+    public class DinoModel
+    {
+        public int Id{ get; set; }
+        public string DinoNameTag { get; set; }
+        public bool IsFemale { get; set; }
+        public string TamedBy { get; set; }
+        public string ImpritedBy { get; set; }
+        public int Level { get; set; }
+        public float DinoImprintQuality { get; set; }
+        public bool IsNeutered { get; set; }
+        public StatModel Stats { get; set; }
+
+    }
+}
---
+++ BreederBot/Models/StatModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreederBot.Models
+{
+    public class StatModel
+    {
+        public float Health { get; set; }
+        public float Stamina { get; set; }
+        public float Torp { get; set; }
+        public float Oxygen { get; set; }
+        public float Food { get; set; }
+        public float Water { get; set; }
+        public float Weight { get; set; }
+        public float Melee { get; set; }
+        public float Movement { get; set; }
+        public float Fortitude { get; set; }
+        public float CraftingSkill { get; set; }
+    }
+}
